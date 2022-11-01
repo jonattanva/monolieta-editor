651a7e8964e97836ca3258e0e52ddb6405eaa681
Jonattan <jonattanva89@gmail.com>
2022-11-01 15:39:56 -0500

    Se agregan nuevas tareas

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+    projectId: 'ai3e19',
     e2e: {
         baseUrl: 'http://localhost:5173/',
         setupNodeEvents(on, config) {
